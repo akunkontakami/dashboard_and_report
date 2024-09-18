7090d57962f59415b3300f67c52e25dfd6e68ca3
laililmahfud <laililmahvut@gmail.com>
2024-09-18 08:11:46 +0700

    cache company

--- app/Http/Controllers/Dashboard/Data/InboundKpiData.php
+++ app/Http/Controllers/Dashboard/Data/InboundKpiData.php
@@ -188,8 +188,7 @@ trait InboundKpiData
           $startDate = date('d/m/Y',strtotime($dates[0])). " 00:00:00";
           $endDate = date('d/m/Y',strtotime($dates[count($dates)-1]))." 23:59:59";
           
-          // return [$startDate,$endDate];
-          return $yeastarApi->getAbaddonMissedCall('9a8e167e-e49b-403e-8a00-421cd13ab24b',$startDate,$endDate);
+          return $yeastarApi->getAbaddonMissedCall($companyId,$startDate,$endDate);
      }
 
      public function kpiWebCall(Request $request, DashboardTicketService $dashboardTicketService)
